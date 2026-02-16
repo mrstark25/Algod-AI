c4dd7134325bc583538465dd7a350ccedac55d4c
Nikhil Baghel <mr_evil25@outlook.com>
2026-02-17 02:31:52 +0530

    update netlify.toml

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    distDir: '.next', // This is usually the output directory
 };
 
 export default nextConfig;
